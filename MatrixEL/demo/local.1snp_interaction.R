fdf0b389fa06a32134d3d9afeed0c66f901b3b87
Yizhen Zhong <yizhenzhong@northwestern.edu>
2017-07-19 10:16:50 -0500

    add interaction

--- MatrixEL/demo/local.1snp_interaction.R
+++ MatrixEL/demo/local.1snp_interaction.R
@@ -13,24 +13,26 @@ cvrt.mat = 2 + matrix(rnorm(n*nc), ncol = nc);
 snps.mat = floor(runif(n, min = 0, max = 3)); 
 local.mat = floor(runif(n, min = 0, max = 3));
 
+# Generate the interaction
+int.mat = snps.mat * local.mat
 # Generate the vectors with genotype and expression variables
 
-gene.mat = cvrt.mat %*% rnorm(nc) + rnorm(n) + 0.5 * snps.mat + 1 + 0.3 *local.mat;
+gene.mat = cvrt.mat %*% rnorm(nc) + rnorm(n) + 0.5 * snps.mat + 0.3 *local.mat + 0.6 * int.mat;
 
 # Create 3 SlicedData objects for the analysis
 snps = SlicedData$new( matrix( snps.mat, nrow = 1 ) );
 genes = SlicedData$new( matrix( gene.mat, nrow = 1 ) );
 cvrts = SlicedData$new( t(cvrt.mat) );
 locals = SlicedData$new( matrix(local.mat,nrow=1))
-
+ints = SlicedData$new( matrix(int.mat,nrow=1))
 # Produce no output files
 filename = NULL; # tempfile()
 
 me = Matrix_eQTL_main(
-  snps = snps,
+  snps = ints,
   gene = genes,
   cvrt = cvrts,
-  local = locals,
+  local = snps,
   output_file_name = filename,
   pvOutputThreshold = 1,
   useModel = modelLOCAL,
@@ -49,9 +51,9 @@ rez = c(beta = beta, tstat = tstat, pvalue = pvalue);
   cat("\n\n Matrix eQTL: \n");
   print(rez);
   cat("\n R summary(lm()) output: \n");
-  lmdl = lm( gene.mat ~ snps.mat + cvrt.mat + local.mat);
-  lmout = summary(lmdl)$coefficients[2,c("Estimate","t value","Pr(>|t|)")];
-  print( lmout );
+  lmdl = lm( gene.mat ~ snps.mat + cvrt.mat + int.mat);
+  lmout = summary(lmdl)$coefficients;
+  print( lmout[nrow(lmout),c("Estimate","t value","Pr(>|t|)")] );
 }
 
 # Results from Matrix eQTL and "lm" must agree
